2d341d2b4e9aa37a04c380bff54ab26563ec0798
shabeer nuhman <shabeer1227@outlook.com>
2023-02-21 14:10:12 +0530

    configuring app

--- app/src/main/java/com/example/diceapp/MainActivity.kt
+++ app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.diceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val button  = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            changeText(textView)
+        }
+
+
+    }
+
+    private fun changeText(textView: TextView?) {
+        if (textView != null) {
+            textView.text = "Hi shabeer"
+        }
     }
 }
